8622f60cd43e5a5b3dc15b6cb17af2d8f30af020
Dimitar Dimitrov <42871796+Whimfoome@users.noreply.github.com>
2021-08-31 15:49:14 +0300

    aspdjasldk;

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
 mod frontend;
+use ureq;
 
 fn main() {
+    mytest();
     frontend::main();
+}
+
+fn mytest() -> Result<(), ureq::Error> {
+    let body: String = ureq::get("https://extensions.gnome.org/extension/1160/dash-to-panel/").call()?.into_string()?;
+    println!("{}", body);
+    Ok(())
 }
